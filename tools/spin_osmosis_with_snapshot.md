f4075de9f03010773e3c8662e6fe3a977e5846bb
hyehan <doe041475@gmail.com>
2021-11-26 08:04:17 +0900

    add spin_osmosis_with_snapshot

---
+++ tools/spin_osmosis_with_snapshot.md
@@ -0,0 +1,15 @@
+## Spin Osmosis Full Node
+
+snapshot by [ChainLayer QuickSync](https://quicksync.io/networks/osmosis.html)
+
+
+#### Spin it : 
+ ```bash
+ ./spin_osmosis_with_snapshot.sh -v vX.Y.Z -m _ _ _
+ ```
+ 
+#### Options :
+ ```bash
+-v : Input Osmosis version you want to install (default "latest version")
+-m : Choose (pruned | default | archive) (default "pruned")
+ ```
--- tools/spin_osmosis_with_snapshot.sh
+++ tools/spin_osmosis_with_snapshot.sh
@@ -1,25 +1,34 @@
-#! /bin/bash -x 
+#! /bin/bash -x  
 set -e 
 
 #PEERS=""
-OSMOSIS_VERSION="$2"
+VERSION=""
+MODE=""
+
+while getopts "v:m:h" option
+	do
+	case $option in
+	v) VERSION="$OPTARG"	;;
+	m) MODE="$OPTARG"	;;
+	h) echo "Spin Osmosis Full Node (with ChainLayer QuickSync Snapshot)
+Usage: ./spin_osmosis_with_snapshot.sh -v vX.Y.Z -m ______
+
+ -v  Input Osmosis version you want to install (default \"latest version\")
+ -m  Choose (pruned | default | archive) (default \"pruned\")" ;;
+	esac
+	done
+
 
 main() {
-	version_check
         basic_setup
         install_go
         osmosis
-        get_snapshot
-#       get_peers
-#       spin_up
+ 	get_snapshot
+        get_peers
+        spin_up
 }
 
-version_check() {
-         if [ -z "$OSMOSIS_VERSION" ]; then
-                echo "--osmosis vX.Y.Z' is required"
-                exit 1
-         fi
-}
+
 
 basic_setup() {
        sudo apt-get update -y && sudo apt-get upgrade -y
@@ -40,8 +49,11 @@ install_go() {
 osmosis(){
         git clone https://github.com/osmosis-labs/osmosis
         cd osmosis
-        git fetch && git checkout $OSMOSIS_VERSION
-        make build
+	if [ -z "$VERSION" ]; then
+                VERSION=$(git describe --tags `git rev-list --tags --max-count=1`)
+	fi
+	git fetch && git checkout $VERSION
+	make build
         ./build/osmosisd init --chain-id osmosis-1 BestDEX
 }
 
@@ -49,15 +61,14 @@ osmosis(){
 # get snapshot from ChainLayer QuickSync
 get_snapshot() {
         sudo apt-get install aria2 liblz4-tool -y
-        DATE=$(date +%Y%m%d) && ((DATE-= 1))
-        FILENAME=osmosis-1-default.$DATE.0510.tar.lz4
-        echo "snapshot date : $DATE"
-
+        if [ -z "$MODE" ]; then
+        	MODE="pruned"
+	fi
+	FILENAME=`curl https://quicksync.io/osmosis.json | jq -r --arg MODE "$MODE" '.[] | select(.network==$MODE)|select (.mirror=="Netherlands")|.filename'`
         cd /$HOME/.osmosisd
         if [[ ! -f $FILENAME || -f $FILENAME.aria2 ]]; then
-                aria2c -x5 https://getsin.quicksync.io/$FILENAME
-        fi
-
+		aria2c -x5 https://get.quicksync.io/$FILENAME
+	fi
         lz4 -d $FILENAME | tar xf -
 }
 
@@ -72,3 +83,4 @@ spin_up() {
 }
 
 main; exit
+
